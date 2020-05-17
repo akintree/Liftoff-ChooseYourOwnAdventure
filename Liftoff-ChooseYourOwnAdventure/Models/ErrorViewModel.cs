1ef5a92f4af27a3d4ad4d025ca7c4a7aa4500fab
Annie Kintree <akintree53@gmail.com>
2020-05-17 13:27:54 -0500

    Add project files.

---
+++ Liftoff-ChooseYourOwnAdventure/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Liftoff_ChooseYourOwnAdventure.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
