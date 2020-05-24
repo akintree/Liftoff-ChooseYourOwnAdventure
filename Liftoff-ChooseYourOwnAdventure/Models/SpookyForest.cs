72b219c9c793f9753aaa3a0d2689a29d7152ac7c
Annie Kintree <akintree53@gmail.com>
2020-05-24 14:29:37 -0500

    Choose a game finished, make/submit choice in progress

---
+++ Liftoff-ChooseYourOwnAdventure/Models/SpookyForest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Liftoff_ChooseYourOwnAdventure.Models
+{
+    public class SpookyForest
+    {
+        public string Story { get; set; }
+        public IList<string> Options { get; set; }
+    }
+}
