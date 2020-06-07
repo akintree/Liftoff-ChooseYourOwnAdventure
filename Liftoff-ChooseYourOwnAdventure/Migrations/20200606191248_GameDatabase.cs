c6633600f6e4dcd053ce9aece206c6b435f6a77f
Annie Kintree <akintree53@gmail.com>
2020-06-06 19:39:21 -0500

    pre-edits

---
+++ Liftoff-ChooseYourOwnAdventure/Migrations/20200606191248_GameDatabase.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Liftoff_ChooseYourOwnAdventure.Migrations
+{
+    public partial class GameDatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "GameID",
+                table: "Games",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Games_GameID",
+                table: "Games",
+                column: "GameID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Games_StoryboardID",
+                table: "Games",
+                column: "StoryboardID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Games_Games_GameID",
+                table: "Games",
+                column: "GameID",
+                principalTable: "Games",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Games_Games_StoryboardID",
+                table: "Games",
+                column: "StoryboardID",
+                principalTable: "Games",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Games_Games_GameID",
+                table: "Games");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Games_Games_StoryboardID",
+                table: "Games");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Games_GameID",
+                table: "Games");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Games_StoryboardID",
+                table: "Games");
+
+            migrationBuilder.DropColumn(
+                name: "GameID",
+                table: "Games");
+        }
+    }
+}
